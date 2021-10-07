a6bbf9f685982c5b1762355c2b9eab3dc6e64fbc
ABHINETWORK1 <88935139+ABHINETWORK1@users.noreply.github.com>
2021-10-08 04:06:38 +0530

    Update __main__.py

--- AdityaPlayer/__main__.py
+++ AdityaPlayer/__main__.py
@@ -18,7 +18,7 @@
 import requests
 from pyrogram import Client as Bot
 
-from SnehabhiServer.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from AdityaPlayer.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 from SnehabhiServer.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
