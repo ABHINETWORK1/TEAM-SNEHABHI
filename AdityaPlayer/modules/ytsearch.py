3ede9bdc9ea90240945e6f8513748bb0a4e9c1bb
ABHINETWORK1 <88935139+ABHINETWORK1@users.noreply.github.com>
2021-10-08 03:48:26 +0530

    Update ytsearch.py

--- AdityaPlayer/modules/ytsearch.py
+++ AdityaPlayer/modules/ytsearch.py
@@ -1,5 +1,5 @@
-# AdityaPlayer (Telegram bot project )
-# Copyright (C)  AdityaHalder
+# SNEHABHI SERVER (Telegram bot project )
+# Copyright (C)  SNEHABHI SERVER
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
