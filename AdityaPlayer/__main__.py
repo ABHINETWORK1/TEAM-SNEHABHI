d82a1ae1c7df51742619558307f2746ca851dff2
ABHINETWORK1 <88935139+ABHINETWORK1@users.noreply.github.com>
2021-10-07 19:32:40 +0530

    Update __main__.py

--- AdityaPlayer/__main__.py
+++ AdityaPlayer/__main__.py
@@ -1,5 +1,5 @@
-# AdityaPlayer (Telegram bot project)
-# Copyright (C)  Aditya Halder
+# SNEHABHI SERVER (Telegram bot project)
+# Copyright (C)  SNEHABHI SERVER
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
