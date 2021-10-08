5fa7ccdd11abfb54c665b5808facd406505455ba
ABHINETWORK1 <88935139+ABHINETWORK1@users.noreply.github.com>
2021-10-08 13:55:53 +0530

    Update converter.py

--- AdityaPlayer/services/converter/converter.py
+++ AdityaPlayer/services/converter/converter.py
@@ -18,7 +18,7 @@
 import asyncio
 from os import path
 
-from SnehabhiServer.helpers.errors import FFmpegReturnCodeError
+from AdityaPlayer.helpers.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
